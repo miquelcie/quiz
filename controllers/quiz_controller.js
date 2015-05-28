d1851e453ee3476a9e64d49f3b9659e0c523bccf
Miquel Gonzalez Cie <mgoncieu@gmail.com>
2015-05-28 12:00:55 +0100

    Primera pregunta correccion

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,11 @@
+exports.question = function(req,res){
+	res.render('quizes/question',{title:'Quiz',pregunta:'Capital de Italia'});
+};
+
+exports.answer = function(req,res){
+	var ret_respuesta='Incorrecto';
+	if (req.query.respuesta==='Roma'){
+		ret_respuesta='Correcto';
+	}
+	res.render('quizes/answer',{title:'Quiz',respuesta:ret_respuesta});
+};
