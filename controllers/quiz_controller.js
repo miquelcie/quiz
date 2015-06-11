bd09ca2d01060251b11b3a257d515824087a505d
Eduardo <eduardo@emenuweb.com>
2015-06-11 00:53:22 -0700

    Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,43 @@
-exports.question = function(req,res){
-	res.render('quizes/question',{title:'Quiz',pregunta:'Capital de Italia'});
+var models = require('../models/models.js');
+
+exports.load = function(req,res,next, quizId){
+
+	models.Quiz.findById(quizId).then(function(quiz){
+			if(quiz){
+				req.quiz=quiz;
+				next();
+			} else { next(new Error('No existe quizId='+quizId));}
+
+	}).catch(function(error){next(error);});
 };
 
+exports.index = function(req,res){
+
+	models.Quiz.findAll().then(function(quizes){
+
+		res.render('quizes/index',{title:'Quiz',quizes:quizes});
+	}).catch(function(error){next(error);});
+};
+
+//Get /quizes/:id
+exports.show = function(req,res){
+	res.render('quizes/show',{title:'Quiz',quiz:req.quiz});
+	/*models.Quiz.findById(req.params.quizId).then(function(quiz){
+		console.log('sss'+quiz.id);
+		res.render('quizes/show',{title:'Quiz',quiz:quiz});
+	}).catch(function(error){next(error);});*/
+};
+
+
+//Get /quizes/:id/answer
+
 exports.answer = function(req,res){
-	var ret_respuesta='Incorrecto';
-	if (req.query.respuesta==='Roma'){
-		ret_respuesta='Correcto';
-	}
-	res.render('quizes/answer',{title:'Quiz',respuesta:ret_respuesta});
+		var ret_respuesta='Incorrecto';
+		if (req.query.respuesta===req.quiz.respuesta){
+			ret_respuesta='Correcto';
+		}
+		res.render('quizes/answer',{title:'Quiz',respuesta:ret_respuesta,quiz:req.quiz});
+
 };
 
 exports.author = function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var quizController=require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  	res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId',quizController.load);
+
 router.get('/author',quizController.author );
-router.get('/quizes/question',quizController.question );
-router.get('/quizes/answer',quizController.answer );
+
+router.get('/quizes',quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show );
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer );
 
 
 
