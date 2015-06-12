c23ce6d67733479f7362f7faf7ec47c32b1f1ed0
Miquel Gonzalez Cie <mgoncieu@gmail.com>
2015-06-11 23:57:59 -0700

    Revert "Modulo 7"
    This reverts commit bd09ca2d01060251b11b3a257d515824087a505d.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,52 +1,13 @@
-var models = require('../models/models.js');
-
-exports.load = function(req,res,next, quizId){
-
-	models.Quiz.findById(quizId).then(function(quiz){
-			if(quiz){
-				req.quiz=quiz;
-				next();
-			} else { next(new Error('No existe quizId='+quizId));}
-
-	}).catch(function(error){next(error);});
+exports.question = function(req,res){
+	res.render('quizes/question',{title:'Quiz',pregunta:'Capital de Italia'});
 };
-
-exports.index = function(req,res){
-	if(req.query.search){
-		var replaced = req.query.search.split(' ').join('%');
-		models.Quiz.findAll({where: ["pregunta like ?", '%'+replaced+'%']}).then(function(quizes){
-
-			res.render('quizes/index',{title:'Quiz',quizes:quizes});
-		}).catch(function(error){next(error);});
-	}else{
-		models.Quiz.findAll().then(function(quizes){
-
-			res.render('quizes/index',{title:'Quiz',quizes:quizes});
-		}).catch(function(error){next(error);});
-
-	}
-};
-
-//Get /quizes/:id
-exports.show = function(req,res){
-
-	res.render('quizes/show',{title:'Quiz',quiz:req.quiz});
-	/*models.Quiz.findById(req.params.quizId).then(function(quiz){
-		console.log('sss'+quiz.id);
-		res.render('quizes/show',{title:'Quiz',quiz:quiz});
-	}).catch(function(error){next(error);});*/
-};
-
-
-//Get /quizes/:id/answer
 
 exports.answer = function(req,res){
-		var ret_respuesta='Incorrecto';
-		if (req.query.respuesta===req.quiz.respuesta){
-			ret_respuesta='Correcto';
-		}
-		res.render('quizes/answer',{title:'Quiz',respuesta:ret_respuesta,quiz:req.quiz});
-
+	var ret_respuesta='Incorrecto';
+	if (req.query.respuesta==='Roma'){
+		ret_respuesta='Correcto';
+	}
+	res.render('quizes/answer',{title:'Quiz',respuesta:ret_respuesta});
 };
 
 exports.author = function(req,res){
