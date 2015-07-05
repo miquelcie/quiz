4c675fbb2189d9c267e9082453a2f5b7f2709346
Miquel Gonzalez Cie <mgoncieu@gmail.com>
2015-07-05 23:44:33 +0100

    Quiz 13: Editar preguntas pasos 1,2 y 3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -81,3 +81,33 @@ exports.create = function(req,res){
 
 
 };
+
+//GET /quizes/:id/edit
+exports.edit = function(req,res){
+	var quiz= req.quiz;
+	res.render('quizes/edit',{quiz:quiz, errors:[]});
+};
+
+//PUT /quizes/:id
+exports.update = function(req,res){
+	req.quiz.pregunta= req.body.quiz.pregunta;
+	reg.quiz.respuesta= req.body.quiz.respuesta;
+
+	quiz
+	.validate()
+	.then (
+		function(err){
+			if(err){
+				res.render('quizes/edit',{quiz:req.quiz,errors:err.errors});
+			} else{
+				req.quiz
+				.save({fields:["pregunta","respuesta"]})
+				.then(function(){
+					res.redirect('/quizes');
+				});
+			}
+		}
+	);
+
+
+};
